090ef3d7796cc4afdbf999f23af637c8c698f678
chetan-anand <anand.chetan93@gmail.com>
2014-12-23 01:14:45 +0530

    Attaches local cam stream on index page

---
+++ views/js/script.js
@@ -0,0 +1,19 @@
+// note: make sure hostname available to all connecting clients
+  // (ie. probably not `localhost`)
+  //rtc.connect('ws://yourserveraddress:8001');
+
+rtc.createStream({"video": true, "audio": true}, function(stream){
+    // get local stream for manipulation
+    rtc.attachStream(stream, 'local');
+});
+  /*rtc.createStream({
+      "video": {"mandatory": {}, "optional": []},
+      "audio": true
+    }, function(stream) {
+      document.getElementById('local').src = URL.createObjectURL(stream);
+      document.getElementById('local').play();
+      //videos.push(document.getElementById('you'));
+      //rtc.attachStream(stream, 'you');
+      //subdivideVideos();
+    });*/
+  // more rtc callbacks are available
