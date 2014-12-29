64cbf1065782e88d11b46d7d12be211bbb8096f0
chetan-anand <anand.chetan93@gmail.com>
2014-12-30 01:30:13 +0530

    Back to easyRtc

--- views/js/script.js
+++ views/js/script.js
@@ -1,4 +1,4 @@
-// note: make sure hostname available to all connecting clients
+/*// note: make sure hostname available to all connecting clients
   // (ie. probably not `localhost`)
   //rtc.connect('ws://yourserveraddress:8001');
 
@@ -6,7 +6,7 @@ rtc.createStream({"video": true, "audio": true}, function(stream){
     // get local stream for manipulation
     rtc.attachStream(stream, 'local');
 });
-  /*rtc.createStream({
+  rtc.createStream({
       "video": {"mandatory": {}, "optional": []},
       "audio": true
     }, function(stream) {
@@ -15,5 +15,5 @@ rtc.createStream({"video": true, "audio": true}, function(stream){
       //videos.push(document.getElementById('you'));
       //rtc.attachStream(stream, 'you');
       //subdivideVideos();
-    });*/
-  // more rtc callbacks are available
+    });
+  // more rtc callbacks are available*/
